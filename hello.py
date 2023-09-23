8f518cc4e86e5f12b9cea3426f29e1e2feb2f951
Jhanavi Gera <jhanavigera@gmail.com>
2023-09-23 18:21:15 -0400

    Reworked Activity 4 due to slight difference in implementation

--- hello.py
+++ hello.py
@@ -3,21 +3,18 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,validators
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
+from wtforms.fields import EmailField
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'ECE444_Lab1'
 
-def custom_validator(form, field):
-    if (((field.data).find('utoronto')) == -1):  
-        raise validators.ValidationError('Email must be a U of T email')
-
 class NameForm(FlaskForm):
  name = StringField('What is your name?', validators=[DataRequired()])
- email = StringField('What is your UofT Email address?', validators=[DataRequired(),Email(),custom_validator])
+ email = StringField('What is your UofT Email address?', validators=[DataRequired(),Email()])
  submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,14 +27,13 @@ def index():
     if old_name is not None and old_name != form.name.data:
         flash('Looks like you have changed your name!')
     if (old_email is not None and old_email != form.email.data):
-            flash('Looks like you have changed your email!')
+        flash('Looks like you have changed your email!')
 
     session['name'] = form.name.data
     session['email'] = form.email.data
 
     return redirect(url_for('index'))
- return render_template('index.html',
- form = form, name = session.get('name'), email = session.get('email'))
+ return render_template('index.html',form = form, name = session.get('name'), email = session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
