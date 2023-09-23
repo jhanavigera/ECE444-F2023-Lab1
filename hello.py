3e412b48fbdae7e371bbb037fec8aebcd09bf48f
Jhanavi Gera <jhanavigera@gmail.com>
2023-09-23 16:46:40 -0400

    reproduced example 4-7

--- hello.py
+++ hello.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+#, Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'ECE444_Lab1'
 
-@app.route('/')
+class NameForm(FlaskForm):
+ name = StringField('What is your name?', validators=[DataRequired()])
+ #email = StringField('What is your UofT Email address?', validators=[Email()])
+ submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
- return render_template('index.html', current_time=datetime.utcnow())
+ form = NameForm()
+ if form.validate_on_submit():
+    old_name = session.get('name')
+    if old_name is not None and old_name != form.name.data:
+        flash('Looks like you have changed your name!')
+    session['name'] = form.name.data
+    return redirect(url_for('index'))
+ return render_template('index.html',
+ form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
